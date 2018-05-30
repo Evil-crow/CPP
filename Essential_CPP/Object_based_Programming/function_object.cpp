a47432204f7704936e0ff578846aebc362bbb4a7
Evil-crow <Evilcrow486@gmail.com>
2018-05-31 00:18:14 +0800

    Complete the function object

--- Essential_CPP/Object_based_Programming/function_object.cpp
+++ Essential_CPP/Object_based_Programming/function_object.cpp
@@ -21,3 +21,42 @@ bool less_than::operator()(int value) const
 {
     return value < lt_data;
 }
+
+int count_less_than(const vector<int> &vec, int comp_value)
+{
+    int count = 0;
+    less_than lt(comp_value);
+
+    for (vector<int>::const_iterator iter = vec.begin(); iter != vec.end(); ++iter)
+        if (lt(*iter))
+            count++;
+    
+    return count;
+}
+
+void print_less_than(const vector<int> &vec, int comp_value)
+{
+    less_than lt(comp_value);
+
+    cout << "In this vector"
+        << " < " << comp_value << ":"
+        << endl;
+    for (vector<int>::const_iterator iter = vec.begin(); iter != vec.end(); ++iter)
+        if (lt(*iter))
+            cout << *iter << " " ;
+    
+    cout << endl;
+}
+
+int main(void)
+{
+    int array[] = {
+        1, 2, 5, 6, 7, 8, 89, 45, 67, 45, 34, 23,
+    };
+
+    vector<int> vec = {array, array+12};
+    cout << count_less_than(vec, 45) << endl;
+    print_less_than(vec, 45);
+
+    return 0;
+}
