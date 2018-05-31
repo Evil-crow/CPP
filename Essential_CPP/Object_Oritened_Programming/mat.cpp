5e7b8cb4e6a524fd7f4efe2dacece3002f528b1d
Evil-crow <Evilcrow486@gmail.com>
2018-06-01 01:03:35 +0800

    A test for mat.h

---
+++ Essential_CPP/Object_Oritened_Programming/mat.cpp
@@ -0,0 +1,17 @@
+/*
+ * @filename:    mat.cpp
+ * @author:      Crow
+ * @date:        06/01/2018 00:46:39
+ * @description:
+ */
+
+#include <iostream>
+#include "mat.h"
+
+int main(void)
+{
+    audioBook audio("NieR:Automata", "Sony", "This is an action TV game");
+    print(audio);
+
+    return 0;
+}
