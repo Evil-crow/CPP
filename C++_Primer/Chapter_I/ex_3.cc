069076fa290a011a91d3ed7dc5efe1ce8e1020f8
Evil-crow <Evilcrow486@gmail.com>
2018-09-04 22:58:57 +0800

    using std::cout

---
+++ C++_Primer/Chapter_I/ex_3.cc
@@ -0,0 +1,15 @@
+/* 
+ * Created By Crow
+ * Date: Tue, 04/09/2018
+ * Description: Chapter_I: 1.3
+ *      1.3: 使用 <iostream> 中的cout, cin对象进行流操作 
+ */
+
+#include <iostream>
+
+int main(void)
+{
+    std::cout << "Hello World" << std::endl;
+
+    return 0;
+}
