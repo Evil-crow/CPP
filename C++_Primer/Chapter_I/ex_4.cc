375877b975e8b654d455a84964b8339b344198f4
Evil-crow <Evilcrow486@gmail.com>
2018-09-04 22:59:31 +0800

    return stream object

---
+++ C++_Primer/Chapter_I/ex_4.cc
@@ -0,0 +1,22 @@
+/* 
+ * Created By Crow
+ * Date: Tue, 04/09/2018
+ * Description: CHapter_I: 1.4 
+ */
+
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main(void)
+{
+    int a, b;
+    cin >> a >> b;
+    cout << "a + b = "
+        << a + b
+        << endl;
+    
+    return 0;
+}
