2e42f7992ea4d32ebac5814b8a19d9dd1661715a
Evil-crow <Evilcrow486@gmail.com>
2018-09-05 17:43:00 +0800

    using the class -- sales_item

---
+++ C++_Primer/Chapter_I/ex_23_24_25.cc
@@ -0,0 +1,14 @@
+/* 
+ * @filename: ex_23_24_25.cc
+ * @author: Crow
+ * @date: Wed, 05/09/2018
+ * @description: Chapter_I: 1.23, 1.24, 1.25
+ *          1.23: 借助temp, 每次判断和上次读入的区别. 基于if-else + while
+ *          1.24: 测试即可
+ *          1.25: 同../Sales_item/Sales_item.h, 使用其进行编译即可.
+ */
+
+int main(void)
+{
+    return 0;
+}
